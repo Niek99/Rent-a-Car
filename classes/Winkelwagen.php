6b5add7111a597455ffee9a2d22ff6ca44dca9b4
niek <emailadres@aventus.nl>
2018-01-24 12:32:54 +0100

    Hierbij de Classes met de functions die we gaan gebruiken

---
+++ classes/Winkelwagen.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niek
+ * Date: 24-1-2018
+ * Time: 12:31
+ */
+
+class Winkelwagen
+{
+    function toevoegen(){
+
+    }
+    function opslaan(){
+
+    }
+}
