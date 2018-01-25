db729bf537cead8dd87043d62983b7e783133d53
Milantenhave <milantenhave@hotmail.com>
2018-01-25 14:57:25 +0100

    halaladfsdf

---
+++ afrekenen.php
@@ -0,0 +1,69 @@
+<?php
+    include "includes/header.php";
+    ?>
+    <html>
+        <body>
+            <div class="container">
+                <h2>Factuur</h2>
+                <br>
+                <ul>
+                    <li>Rent-a-Car</li>
+                    <li>Autopad 12</li>
+                    <li>APELDOORN</li>
+                    <li>Telefoon: 0523-123012</li>
+                </ul><br><br>
+                <table style="width:100%">
+                    <tr>
+                        <td>Datum: </td> <td></td>
+                    </tr>
+                    <tr>
+                        <td>Factuurnummer:</td><td></td>
+                    </tr>
+                    <tr>
+                        <td>Behandelaar:</td><td></td>
+                    </tr>
+                </table> <br><br>
+                <ul>
+                    <li>Naam</li>
+                    <li>Adres</li>
+                    <li>Woonplaats/postcode</li>
+                </ul><br>
+                <h3>Reservering</h3><br><br>
+
+                <table style="width:100%;"border="1">
+                    <tr>
+                        <th>Kenteken</th>
+                        <th>Merk</th>
+                        <th>Type</th>
+                        <th>Gereserveerde periode</th>
+                        <th>Prijs per dag</th>
+                        <th>Totaal</th>
+                    </tr>
+                    <tr>
+                        <td>-kenteken-</td>
+                        <td>-Merk-</td>
+                        <td>-type-</td>
+                        <td>-Gereserveerde periode-</td>
+                        <td>-Prijs per dag-</td>
+                        <td>-Totaal-</td>
+                    </tr>
+                    <tr>
+                        <td>-kenteken-</td>
+                        <td>-Merk-</td>
+                        <td>-type-</td>
+                        <td>-Gereserveerde periode-</td>
+                        <td>-Prijs per dag-</td>
+                        <td>-Totaal-</td>
+                    </tr>
+                </table><br><br>
+                <p>
+                    Betalingen dienen plaats te vinden veertien dagen voor de aanvang van de gereserveerde periode<br>
+                    Betalingen kunnen gedaan worden via Ideal of worden gestort op rekeningnummer 3210808<br>
+                    Indien er gereserveerd is binnen veerdien dagen voor de aanvang van de gereserveerde periode,<br>
+                    Dient de betaling direct plaats te vinden.
+                </p>
+
+            </div>
+        </body>
+    </html>
+
