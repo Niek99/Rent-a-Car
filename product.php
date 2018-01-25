172e9e955d18ae0cb68a6813bb3d40eeefc26b56
Milantenhave <milantenhave@hotmail.com>
2018-01-25 12:15:58 +0100

    halaladfsdf

--- product.php
+++ product.php
@@ -41,7 +41,7 @@
         <div class="container">
             <div class="container1">
                 <div class="product">
-                    <img class="product_picture col-sm-6" src="assets/img/Audia4.jpg">
+                    <img class="product_picture col-sm-12" src="assets/img/Audia4.jpg">
                     <div class="product_text col-sm-6">
                         Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit tortor eget felis porttitor volutpat. Nulla porttitor accumsan tincidunt. Donec rutrum congue leo eget malesuada. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Cras ultricies ligula sed magna dictum porta.
 
@@ -50,12 +50,9 @@
                         Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Proin eget tortor risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit tortor eget felis porttitor volutpat. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt.
 
                         Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Donec sollicitudin molestie malesuada. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Donec rutrum congue leo eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Nulla quis lorem ut libero malesuada feugiat.
-
-                        Curabitur aliquet quam id dui posuere blandit. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Sed porttitor lectus nibh. Curabitur aliquet quam id dui posuere blandit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Donec sollicitudin molestie malesuada. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Pellentesque in ipsum id orci porta dapibus. Donec rutrum congue leo eget malesuada.
                     </div>
-                    <div class="lenen_box col-sm-2 col-md-2">
+                    <div class="lenen_box col-sm-6 col-md-6">
                         <div  class="thumbnail">
-                            <img class="topcar" src="assets/img/BMW.png" alt="...">
                             <div class="caption" style="text-align: center">
                                 <h3>Audi A4 2016</h3>
                                 <p>Maar 500 euro per maand!</p>
@@ -65,10 +62,11 @@
                                 <input type="text" id="from" name="from"><br>
                                 <label for="to">Tot:</label>
                                 <input type="text" id="to" name="to"><br>
+                                <input type="hidden">11-PO-TT</input><br>
                                 <button style="margin-top: 5px;" type="submit">Verder</button>
                             </form>
                         </div>
-                </div>
+                    </div>
             </div>
         </div>
     </body>
