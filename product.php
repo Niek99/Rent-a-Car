6744ac124b84cc2d7fdfac74d04c27133f2cd115
Milantenhave <milantenhave@hotmail.com>
2018-01-25 09:09:56 +0100

    halaladfsdf

---
+++ product.php
@@ -0,0 +1,21 @@
+<?php
+    include "includes/header.php";
+    include "includes/head.php";
+?>
+<html>
+    <body>
+        <div class="product">
+            <img class="product_picture" src="assets/img/volkswagen.jpg">
+            <div class="product_text">
+                hallo
+            </div>
+        </div>
+    </body>
+</html>
+<?php
+    include "includes/footer.php";
+?>
+<div class="ingelogd" ">
+    <h1>U bent succesvol ingelogd!</h1>
+    <button>Klik hier om terug te gaan naar de homepagina!</button>
+</div>
